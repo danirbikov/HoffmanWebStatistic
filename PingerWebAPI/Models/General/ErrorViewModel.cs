2d3db8b6460b934487758bac85e37023327892a8
danirbikov <91866386+danirbikov@users.noreply.github.com>
2023-09-19 10:17:38 +0300

    Delete PingerWebAPI directory

--- PingerWebAPI/Models/General/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PingerAPI.Models.General
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
